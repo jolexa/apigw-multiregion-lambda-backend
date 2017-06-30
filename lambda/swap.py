807a2b31b3b8d4c3e28411fa94e9f10bc4c5f818
Jeremy Olexa <jolexa@jolexa.net>
2017-06-30 08:50:26 -0500

    Add comments

--- lambda/swap.py
+++ lambda/swap.py
@@ -1,2 +1,17 @@
+#!/usr/bin/env python
+
 def handler(event, context):
+    '''
+    - Receive an alarm. determine if the alarm is for the primary or not
+    - update the primary infra stack to be the new standby
+    - update the primary ping pong stack to be the new standby
+    - update the standby infra stack to be the new primary
+    - update the standby ping-pong stack to be the new primary
+    '''
+
+    '''
+    MyStack
+    OtherStack
+    '''
+
     print(event)

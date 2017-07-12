686aedf15b401f7f6def6cefc365ce9a03b8fb58
Jeremy Olexa <jolexa@jolexa.net>
2017-07-11 19:31:13 -0500

    Split into ordered functions

--- lambda/swap.py
+++ lambda/swap.py
@@ -45,7 +45,7 @@ def check_green_light(stackname, region='ca-central-1'):
     return True
 
 
-def handler(event, context):
+def kicker(event, context):
     '''
     Think about it, if the $current_primary stack ponger is not getting invoked, it
     means that lambda is degraded in the primary region. So, it makes sense to
@@ -66,50 +66,75 @@ def handler(event, context):
     5) update the mystack infra stack to be the new primary
     6) update the mystack ping-pong stack to be the new primary
     '''
-    print(event)
 
+def FirstFunction(event, context):
+    print(event)
     # 1) update the mystack infra stack to be the transitional url
     update_stack(
             os.environ['MyInfraStackName'],
             os.environ['TransitionalUrl'],
             os.environ['AWS_DEFAULT_REGION']
             )
+
+def FirstWait(event, context):
+    print(event)
     # Do not pass go until MyInfraStackName is done (route53 takes longer than
     # ping pong stack)
     check_green_light(os.environ['MyInfraStackName'],
             os.environ['AWS_DEFAULT_REGION'])
+
+def SecondFunction(event, context):
+    print(event)
     # 2) update the mystack ping-pong stack to be the transitional url
     update_stack(
             os.environ['MyPingPongStackName'],
             os.environ['TransitionalUrl'],
             os.environ['AWS_DEFAULT_REGION']
             )
+
+def ThirdFunction(event, context):
+    print(event)
     # 3) update the otherstack infra stack to be the new standby
     update_stack(
             os.environ['OtherInfraStackName'],
             os.environ['StandbyUrl'],
             os.environ['OtherStackRegion']
             )
+
+def ThirdWait(event, context):
+    print(event)
     # Do not pass go until OtherInfraStackName is done (route53 takes longer
     # than ping pong stack)
     check_green_light(os.environ['OtherInfraStackName'],
             os.environ['OtherStackRegion'])
+
+def ForthFunction(event, context):
+    print(event)
     # 4) update the otherstack ping pong stack to be the new standby
     update_stack(
             os.environ['OtherPingPongStackName'],
             os.environ['StandbyUrl'],
             os.environ['OtherStackRegion']
             )
+
+def FifthFunction(event, context):
+    print(event)
     # 5) update the mystack infra stack to be the new primary
     update_stack(
             os.environ['MyInfraStackName'],
             os.environ['PrimaryUrl'],
             os.environ['AWS_DEFAULT_REGION']
             )
+
+def FifthWait(event, context):
+    print(event)
     # Do not pass go until MyInfraStackName is done (route53 takes longer than
     # ping pong stack)
     check_green_light(os.environ['MyInfraStackName'],
             os.environ['AWS_DEFAULT_REGION'])
+
+def SixthFunction(event, context):
+    print(event)
     # 6) update the mystack ping-pong stack to be the new primary
     update_stack(
             os.environ['MyPingPongStackName'],
